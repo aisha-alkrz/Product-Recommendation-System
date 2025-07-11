927f8c3c6aa46f5e39cd49bd328711c7d4f1cc6c
aish-alkrz <aishaalkrz@gmail.com>
2025-07-11 15:46:49 +0300

    create table categories

--- scripts/create_database.py
+++ scripts/create_database.py
@@ -1,5 +1,11 @@
 import sqlite3 as sql;
 
 conn = sql.connect("data/products.db")
-corsur = conn.cursor()
+cursor = conn.cursor()
 
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS Categories (
+    category_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL
+);
+""")
