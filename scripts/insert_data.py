be4f914169e0f6bc3022f4de4fb7cd1032ef5491
aish-alkrz <aishaalkrz@gmail.com>
2025-07-11 16:18:14 +0300

    Insert data for table categories

--- scripts/insert_data.py
+++ scripts/insert_data.py
@@ -3,3 +3,15 @@ import sqlite3 as sql
 # Connect to the database
 conn = sql.connect("data/products.db")
 cursor = conn.cursor()
+
+
+# Insert categories 
+
+categories = [
+    ("Electronics",),
+    ("Cosmetics",),
+    ("Furniture",)
+]
+
+cursor.executemany("INSERT INTO Categories (name) VALUES (?)", categories)
+conn.commit()
