3a31a906d28583ab0f3b99b8947613630f12f72c
aish-alkrz <aishaalkrz@gmail.com>
2025-07-11 15:49:06 +0300

    create table products

--- scripts/create_database.py
+++ scripts/create_database.py
@@ -9,3 +9,17 @@ CREATE TABLE IF NOT EXISTS Categories (
     name TEXT NOT NULL
 );
 """)
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS Products (
+    product_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    price REAL NOT NULL,
+    brand TEXT,
+    type TEXT,
+    color TEXT,
+    category_id INTEGER,
+    cluster_id_no_price INTEGER,
+    FOREIGN KEY (category_id) REFERENCES Categories(category_id)
+);
+""")
